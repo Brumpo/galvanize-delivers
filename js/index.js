0f5e3ebf345878be5e81824bd765e9f19d4c8a92
Brent Schroder <schroder.brent@gmail.com>
2017-10-26 19:19:22 -0600

    polished for pushing to surge

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
   var $cardaction=$('.card-action');
+  var $form=$('#myForm')[0];
   var tablect = 0;
+  var subtotal = 0;
+  const tax= 0.08;
   var renderCard = function(e){;
     if(e.target.value){
       let itemarray=e.target.value.split('-')
@@ -8,14 +11,36 @@ $(document).ready(function(){
       let itemprice=itemarray[1];
       let $tableitem=$('.tablename');
       let $tableprice=$('.tableprice');
+      let $tablesubtotal=$('#subtotal')[0];
+      let $tabletax=$('#tax')[0];
+      let $tabletotal=$('#total')[0];
+      $($tablesubtotal).empty()
+      $($tabletax).empty()
+      $($tabletotal).empty()
       $tableitem[tablect].append(itemname);
       $tableprice[tablect].append(itemprice);
       tablect++;
+      subtotal+=parseFloat(itemprice);
+      $tablesubtotal.append(subtotal.toFixed(2));
+      $tabletax.append((tax*subtotal).toFixed(2));
+      $tabletotal.append((subtotal+(subtotal*tax)).toFixed(2));
     }
   }
-  for(let i=0;i<$cardaction.length;i++){
-    console.log($cardaction[i]);
-    $($cardaction[i]).on('click',renderCard)
+  var submit = function(e){
+    e.preventDefault();
+    let $teleform = $('#icon_telephone');
+    let regexer= $($teleform).val().search(/^(\+0?1\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/g)
+    if(regexer===(-1)){
+      alert('please enter Phone number as ### ### ####')
+    }else{
+      alert('Par-Se-ing your order! Tres Bien!')
+      location.reload();
     }
 
+  }
+  for(let i=0;i<$cardaction.length;i++){
+    console.log($cardaction[0]);
+    $($cardaction[i]).on('click',renderCard)
+    }
+  $($form).on('submit',submit)
 });
